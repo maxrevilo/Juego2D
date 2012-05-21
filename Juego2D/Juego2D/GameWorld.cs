96f28c6166b9d66eb93a9dc039f06ae573cff911
Oliver Perez <oliver.a.perez.c@gmail.com>
2012-05-20 22:08:20 -0430

    Graficado de fisicas y Personage moviendose como un gato.

---
+++ Juego2D/Juego2D/GameWorld.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using FarseerPhysics;
+using FarseerPhysics.Dynamics;
+
+namespace Juego2D
+{
+    public class GameWorld : World
+    {
+        public Camera2D camera;
+        public float airDrag;
+
+        public GameWorld(Vector2 gravity, float airDrag, Camera2D camera)
+            : base(gravity)
+        {
+            this.camera = camera;
+            this.airDrag = airDrag;
+            this.BodyAdded += BodyAddedHandler;
+        }
+
+        private void BodyAddedHandler(Body body)
+        {
+            body.LinearDamping = airDrag;
+        }
+    }
+}
